910a61a618ee7135f839dd4d7f91679f93d53b8d
T.Yokoi <laylalayla2905@gmail.com>
2021-10-21 16:06:46 +0200

    created base for this app

---
+++ src/inlamningsuppgift2/App.java
@@ -0,0 +1,14 @@
+package inlamningsuppgift2;
+
+import javax.swing.*;
+
+public class App extends JFrame {
+    JPanel gameBoardPanel = new JPanel();
+    JButton resetButton = new JButton();
+
+    App() {
+        System.out.println("Start the application!");
+
+    }
+
+}
---
+++ src/inlamningsuppgift2/Tile.java
@@ -0,0 +1,7 @@
+package inlamningsuppgift2;
+
+import javax.swing.*;
+
+public class Tile extends JButton {
+
+}
